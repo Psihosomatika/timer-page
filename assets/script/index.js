1de240fdbf6238f011f7236cba31bda381e5ce9c
Diana Rukavishnikova <psihomatika@gmail.com>
2024-06-25 21:41:56 +0200

    feat: add timer

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,20 +1,32 @@
+const jsSeconds = document.querySelector(".js-seconds");
+const jsMinutes = document.querySelector(".js-minutes");
+const timerBlock = document.querySelector(".header__timer-block");
 const popup = document.querySelector(".popup");
 const closeBtn = document.querySelector(".popup__close");
+
 closeBtn.addEventListener("click", () => {
   popup.classList.remove("popup_visable");
 });
 // popup_visable
-let twoMinutes = 120;
+//120
+let twoMinutes = 40;
 const timer = () => {
-  const minets = twoMinutes / 60;
-  const second = twoMinutes;
-  console.log("минуты");
-  console.log(minets);
-  console.log("секунды");
+  const minets = Math.trunc(twoMinutes / 60);
+  jsMinutes.textContent = `0${minets}`;
+  if (twoMinutes % 60 == 0) {
+    jsSeconds.textContent = "00";
+  }
+  const seconds = twoMinutes - minets * 60;
+  if (seconds < 31) {
+    timerBlock.classList.add("header__timer-block_warning");
+  }
+  if (seconds > 9) {
+    jsSeconds.textContent = `${seconds}`;
+  } else {
+    jsSeconds.textContent = `0${seconds}`;
+  }
   twoMinutes -= 1;
-  console.log(twoMinutes);
-  // 120000 - 2 минуты
-  if (twoMinutes === 0) {
+  if (twoMinutes === -1) {
     clearInterval(intervalID);
   }
 };
